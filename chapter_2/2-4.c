bbc42ff3a1db195fe21643238dfc5fce6ac727e0
Greg Anderson <greg@infobytes.com>
2016-01-04 09:18:47 +0000

    Added a simple contains function.

--- chapter_2/2-4.c
+++ chapter_2/2-4.c
@@ -17,3 +17,15 @@ void squeeze_alt(char s1[], char s2[]) {
 	}
 	s[j] = '\0';
 }
+
+/* Returns 1 if s contains c, 0 otherwise */
+int contains(char s[], char c) {
+	int i;
+	i = 0;
+	while (s[i] != '\0') {
+		if (s[i] == c)
+			return 1;
+		i++;
+	}
+	return 0;
+}
